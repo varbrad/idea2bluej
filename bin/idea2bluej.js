f98913ebe33d7460005a983e2b419a44e4eeb6b8
Brad Davies <varbrad@users.noreply.github.com>
2017-02-08 23:03:17 +0000

    Optimizations to system
    Code rewritten to be functional within index rather than relying on 'bin' node script to do logic (Simply executes in order and checks for errors).
    
    Remembers paths, reducing need to pass them to functions everytime.

--- bin/idea2bluej.js
+++ bin/idea2bluej.js
@@ -3,7 +3,6 @@
 const idea2bluej = require('../index')
 
 const chalk = require('chalk')
-const path = require('path')
 const version = require('../package.json').version
 
 console.log(chalk.blue('IDEA2BlueJ v' + version + '\n'))
@@ -28,59 +27,37 @@ function log (message) {
 function run () {
   let start = process.hrtime()
 
-  if (!argv.projectIn) {
-    return error('No IntelliJ IDEA project folder specified!')
-  }
+  if (!argv.projectIn) return error('No IntelliJ IDEA project folder specified!')
+  if (!argv.projectOut) return error('No BlueJ output folder specified!')
 
-  // First, join input/output paths
-  log('Resolving file paths...')
-  const input = path.join(process.cwd(), argv.projectIn)
-  const inputLibsPath = path.join(process.cwd(), argv.projectIn, argv.libs)
-  const inputSrcPath = path.join(process.cwd(), argv.projectIn, argv.src)
-  const output = path.join(process.cwd(), argv.projectOut)
+  // Step 1 - Validate paths & setup
+  log('Validating project structure...')
+  if (!idea2bluej.validateInput(argv.projectIn)) return error('IntelliJ IDEA project folder not valid!')
+  if (!idea2bluej.validateOutput(argv.projectOut)) return error('BlueJ output folder not valid!')
+  if (!idea2bluej.validateSource(argv.src)) return error('IntelliJ IDEA source folder not valid!')
+  if (!idea2bluej.validateLibs(argv.libs)) return error('IntelliJ IDEA libs folder not valid!')
 
-  // Ensure all paths exist
-  if (!idea2bluej.isDirectory(input)) return error('IntelliJ IDEA project folder "' + input + '" not found!')
-  if (!idea2bluej.isDirectory(inputLibsPath)) return error('IntelliJ IDEA libs folder "' + inputLibsPath + '" not found!')
-  if (!idea2bluej.isDirectory(inputSrcPath)) return error('IntelliJ IDEA source folder "' + inputSrcPath + '" not found!')
-
-  // Attempt to clone project structure
+  // Step 2 - Clone project structure
   log('Cloning project structure...')
-  if (!idea2bluej.cloneSource(input, output)) {
-    // Something went wrong, show an error
-    return error('Could not clone source path "' + input + '" to "' + output + '"!')
-  }
+  if (!idea2bluej.cloneProject()) return error('Could not clone IntelliJ IDEA project!')
 
-  // Resolve generated paths
-  log('Resolving generated file paths...')
-  const outputLibsPath = path.join(process.cwd(), argv.projectOut, argv.libs)
-  const outputSrcPath = path.join(process.cwd(), argv.projectOut, argv.src)
+  // Step 3 - Rename libs folder
+  log('Renaming "' + argv.libs + '" folder to "+libs"...')
+  if (!idea2bluej.renameLibs()) return error('Could not rename "' + argv.libs + '" folder!')
 
-  // Rename libs folder to '+libs'
-  log('Renaming library folder...')
-  if (!idea2bluej.renameDir(outputLibsPath, path.join(path.dirname(outputLibsPath), '/+libs'))) {
-    // Couldnt rename file
-    return error('Could not rename libs path "' + outputLibsPath + '" to "+libs"!')
-  }
+  // Step 4 - Inject empty 'package.bluej' folders throughout src
+  log('Injecting "package.bluej" files into source packages...')
+  if (!idea2bluej.injectPackages()) return error('Could not inject BlueJ packages!')
 
-  // Inject package.bluej folders
-  log('Injecting BlueJ project package files...')
-  if (!idea2bluej.injectBlueJPackages(outputSrcPath)) {
-    return error('Could not inject BlueJ project package files!')
-  }
+  // Step 5 - Move all contents of src up a directory
+  log('Moving "' + argv.src + '" contents into project root source path...')
+  if (!idea2bluej.moveSource()) return error('Could not move "' + argv.src + '" contents into project root!')
 
-  // Move all folders within 'src' up a directory
-  log('Moving source directory up...')
-  if (!idea2bluej.moveAllInDir(outputSrcPath, path.dirname(outputSrcPath))) {
-    return error('Could not move source path "' + outputSrcPath + '" to project root directory!')
-  }
+  // Step 6 - Remove all .idea/.git/.iml files/folders
+  log('Cleaning generated BlueJ project...')
+  if (!idea2bluej.cleanProject()) return error('Could not clean generated project!')
 
-  // 7, Remove .iml file and '.idea' folder
-  log('Removing IntelliJ IDEA project files...')
-  if (!idea2bluej.removeProjectFiles(output)) {
-    return error('Could not remove IntelliJ IDEA project files!')
-  }
-
+  // Step 7 - Done!
   let end = process.hrtime(start)
   let seconds = end[0] + (end[1] * 1e-9)
   console.log(chalk.green('Project converted in ' + seconds.toFixed(3) + 's'))
--- index.js
+++ index.js
@@ -1,139 +1,99 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-/**
- * Clones a directory from a source directory to an output directory
- * @returns true if cloned succesfully, else false
- */
-function cloneSource (sourceDir, outputDir) {
-  // Check to ensure the sourceDir exists
-  if (fs.existsSync(sourceDir)) {
-    // Empty the outputDir
-    fs.emptyDirSync(outputDir)
-    // Copy the source to outputDir
-    fs.copySync(sourceDir, outputDir)
-    // Succesfully copied directory
-    return true
-  }
-  return false
-}
-
-/**
- * If a directory exists
- * @returns true if directory exists, else false
- */
-function isDirectory (path) {
-  return fs.existsSync(path) && fs.statSync(path).isDirectory()
-}
-
-/**
- * If a file exists
- * @returns true if directory exists, else false
- */
-function isFile (path) {
-  return fs.existsSync(path) && fs.statSync(path).isFile()
-}
-
-/**
- * Renames a directory
- * @returns true if renamed succesfully, else false
- */
-function renameDir (oldPath, newPath) {
-  // Only attempt to rename if path exists and is directory
-  if (fs.existsSync(oldPath) && fs.statSync(oldPath).isDirectory()) {
-    fs.renameSync(oldPath, newPath)
-    return true
-  }
-  return false
-}
-
-/**
- * Moves a directory
- * @returns true if moved succesfully, else false
- */
-function moveDir (oldPath, newPath) {
-  // Only attempt to rename if path exists and is directory
-  if (fs.existsSync(oldPath) && fs.statSync(oldPath).isDirectory()) {
-    fs.copySync(oldPath, newPath)
-    fs.removeSync(oldPath)
-    return true
-  }
-  return false
-}
-
-/**
- * Moves all files/directories in a directory to a new directory
- * @returns true if moved succesfully, else false
- */
-function moveAllInDir (oldPath, newPath) {
-  // Only attempt to rename if path exists and is directory
-  if (fs.existsSync(oldPath) && fs.statSync(oldPath).isDirectory()) {
-    // Loop thru all files in this folder
-    let files = fs.readdirSync(oldPath)
-    for (let i = 0; i < files.length; ++i) {
-      let filePath = path.join(oldPath, files[i])
-      fs.copySync(filePath, path.join(newPath, files[i]))
-      fs.removeSync(filePath)
-    }
-    fs.removeSync(oldPath)
-    return true
-  }
-  return false
-}
-
-function injectBlueJPackages (output) {
-  let dirs = [output]
-  while (dirs.length > 0) {
-    // Get list of current directories within dirs[0]
-    let files = fs.readdirSync(dirs[0])
-    for (let i = 0; i < files.length; ++i) {
-      let p = path.join(dirs[0], files[i])
-      if (fs.statSync(p).isDirectory()) {
-        dirs.push(p)
-      }
-    }
-    // Add 'package.bluej' to this path
-    fs.writeFileSync(path.join(dirs[0], 'package.bluej'), '')
-
-    dirs.shift()
-  }
-  return true
-}
-
-function removeProjectFiles (output) {
-  if (fs.existsSync(output) && fs.statSync(output).isDirectory()) {
-    let files = fs.readdirSync(output)
-    for (let i = 0; i < files.length; ++i) {
-      let name = files[i]
-      if (name.match(/(\.iml)|(\.idea)|(\.git)$/)) {
-        fs.removeSync(path.join(output, name))
-      }
-    }
-    return true
-  }
-  return false
-}
-
-/**
- * Deletes a file or directory.
- * @returns true if removed succesfully, else false
- */
-function deletePath (filepath) {
-  if (fs.existsSync(filepath)) {
-    fs.removeSync(filepath)
-    return true
-  }
-  return false
-}
-
 module.exports = {
-  cloneSource: cloneSource,
-  deletePath: deletePath,
-  isDirectory: isDirectory,
-  isFile: isFile,
-  moveDir: moveDir,
-  moveAllInDir: moveAllInDir,
-  renameDir: renameDir,
-  injectBlueJPackages: injectBlueJPackages,
-  removeProjectFiles: removeProjectFiles
+
+  input: null,
+  output: null,
+  src: null,
+  libs: null,
+
+  validateInput: function (uri) {
+    this.input = path.join(process.cwd(), uri)
+    return this.validateDir(this.input)
+  },
+
+  validateOutput: function (uri) {
+    this.output = path.join(process.cwd(), uri)
+    return true
+  },
+
+  validateSource: function (uri) {
+    this.src = uri
+    return this.validateDir(path.join(this.input, uri))
+  },
+
+  validateLibs: function (uri) {
+    this.libs = uri
+    return this.validateDir(path.join(this.input, uri))
+  },
+
+  validateDir: function (uri) {
+    return fs.existsSync(uri) && fs.statSync(uri).isDirectory()
+  },
+
+  /**
+   * Clones the project structure from input to output
+   * @returns true if cloned succesfully, else false
+   */
+  cloneProject: function () {
+    if (this.input && this.output) {
+      fs.emptyDirSync(this.output)
+      fs.copySync(this.input, this.output)
+      return true
+    }
+    return false
+  },
+
+  renameLibs: function () {
+    if (this.output && this.libs) {
+      fs.renameSync(path.join(this.output, this.libs), path.join(this.output, '/+libs'))
+      return true
+    }
+    return false
+  },
+
+  injectPackages: function () {
+    if (this.output && this.src) {
+      let dirs = [path.join(this.output, this.src)]
+      while (dirs.length > 0) {
+        let files = fs.readdirSync(dirs[0])
+        for (let i = 0; i < files.length; ++i) {
+          let p = path.join(dirs[0], files[i])
+          if (fs.statSync(p).isDirectory()) dirs.push(p)
+        }
+        fs.writeFileSync(path.join(dirs[0], 'package.bluej'), '')
+        dirs.shift()
+      }
+      return true
+    }
+    return false
+  },
+
+  moveSource: function () {
+    if (this.output && this.src) {
+      let src = path.join(this.output, this.src)
+      let files = fs.readdirSync(src)
+      for (let i = 0; i < files.length; ++i) {
+        let p = path.join(src, files[i])
+        fs.copySync(p, path.join(this.output, files[i]))
+        fs.removeSync(p)
+      }
+      fs.removeSync(src)
+      return true
+    }
+    return false
+  },
+
+  cleanProject: function () {
+    if (this.output) {
+      let files = fs.readdirSync(this.output)
+      for (let i = 0; i < files.length; ++i) {
+        let name = files[i]
+        if (name.match(/(\.iml)|(\.idea)|(\.git)$/g)) fs.removeSync(path.join(this.output, name))
+      }
+      return true
+    }
+    return false
+  }
 }
